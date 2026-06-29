57cfd1f80f1a7c5e03daa807b74750841f4d9713
ACDPDEV <iamacdp.dev@gmail.com>
2026-06-29 18:01:36 -0500

    docs(common): add Doxygen documentation to rgb and color functions in Color.h

--- Common/Color.h
+++ Common/Color.h
@@ -3,7 +3,11 @@
 #include <string>
 using namespace std;
 
-inline string color(array<int, 3> foreground, int background[]) {
+/// Generates an ANSI escape sequence for colored terminal output.
+/// @param foreground Foreground RGB color array [r, g, b] (0-255 each)
+/// @param background Background RGB color array [r, g, b] (0-255 each)
+/// @return ANSI escape sequence string for 24-bit true color (foreground + background)
+string color(int foreground[3], int background[3]) {
     return
         "\033[38;2;"
         + to_string(foreground[0]) + ";"
