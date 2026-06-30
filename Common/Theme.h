7fed8d4b124278a0b6e4739de3aace12ae4ac822
ACDPDEV <iamacdp.dev@gmail.com>
2026-06-30 09:50:14 -0500

    feat: Theme.h con array<int,3> const y Doxygen completo
    - 22 arrays como const array<int, 3>
    - Documentación Doxygen en cada color
    - Incluye <array> y using namespace std

--- Common/Theme.h
+++ Common/Theme.h
@@ -1,71 +1,75 @@
 // Matte Black Theme desde Omarchy
 // https://omarchytheme.com/themes/matte-black/
 
+#include <array>
+
+using namespace std;
+
 // Base
 /// @brief Color de fondo base (#121212)
-int BACKGROUND[3] = {18, 18, 18};
+const array<int, 3> BACKGROUND = {18, 18, 18};
 
 /// @brief Color de primer plano / texto principal (#bebebe)
-int FOREGROUND[3] = {190, 190, 190};
+const array<int, 3> FOREGROUND = {190, 190, 190};
 
 /// @brief Color de acento / highlight (#e68e0d)
-int ACCENT[3] = {230, 142, 13};
+const array<int, 3> ACCENT = {230, 142, 13};
 
 /// @brief Color del cursor (#eaeaea)
-int CURSOR[3] = {234, 234, 234};
+const array<int, 3> CURSOR = {234, 234, 234};
 
 /// @brief Fondo de selección (#515151)
-int SELECTION_BACKGROUND[3] = {81, 81, 81};
+const array<int, 3> SELECTION_BACKGROUND = {81, 81, 81};
 
 /// @brief Texto de selección (#bebebe)
-int SELECTION_FOREGROUND[3] = {190, 190, 190};
+const array<int, 3> SELECTION_FOREGROUND = {190, 190, 190};
 
 // Normal colors (0-7) - ANSI standard colors
 /// @brief Color 0: Negro (#333333)
-int COLOR0[3] = {51, 51, 51};
+const array<int, 3> COLOR0 = {51, 51, 51};
 
 /// @brief Color 1: Rojo (#D35F5F)
-int COLOR1[3] = {211, 95, 95};
+const array<int, 3> COLOR1 = {211, 95, 95};
 
 /// @brief Color 2: Amarillo/Verde (#FFC107)
-int COLOR2[3] = {255, 193, 7};
+const array<int, 3> COLOR2 = {255, 193, 7};
 
 /// @brief Color 3: Rojo oscuro (#b91c1c)
-int COLOR3[3] = {185, 28, 28};
+const array<int, 3> COLOR3 = {185, 28, 28};
 
 /// @brief Color 4: Naranja/Acento (#e68e0d)
-int COLOR4[3] = {230, 142, 13};
+const array<int, 3> COLOR4 = {230, 142, 13};
 
 /// @brief Color 5: Magenta/Rojo (#D35F5F)
-int COLOR5[3] = {211, 95, 95};
+const array<int, 3> COLOR5 = {211, 95, 95};
 
 /// @brief Color 6: Cian/Blanco suave (#bebebe)
-int COLOR6[3] = {190, 190, 190};
+const array<int, 3> COLOR6 = {190, 190, 190};
 
 /// @brief Color 7: Blanco (#bebebe)
-int COLOR7[3] = {190, 190, 190};
+const array<int, 3> COLOR7 = {190, 190, 190};
 
 // Bright colors (8-15) - ANSI bright variants
 /// @brief Color 8: Gris medio (#8a8a8d)
-int COLOR8[3] = {138, 138, 141};
+const array<int, 3> COLOR8 = {138, 138, 141};
 
 /// @brief Color 9: Rojo brillante (#B91C1C)
-int COLOR9[3] = {185, 28, 28};
+const array<int, 3> COLOR9 = {185, 28, 28};
 
 /// @brief Color 10: Amarillo brillante (#FFC107)
-int COLOR10[3] = {255, 193, 7};
+const array<int, 3> COLOR10 = {255, 193, 7};
 
 /// @brief Color 11: Rojo muy oscuro (#b90a0a)
-int COLOR11[3] = {185, 10, 10};
+const array<int, 3> COLOR11 = {185, 10, 10};
 
 /// @brief Color 12: Naranja brillante (#f59e0b)
-int COLOR12[3] = {245, 158, 11};
+const array<int, 3> COLOR12 = {245, 158, 11};
 
 /// @brief Color 13: Rojo brillante (#B91C1C)
-int COLOR13[3] = {185, 28, 28};
+const array<int, 3> COLOR13 = {185, 28, 28};
 
 /// @brief Color 14: Blanco suave (#eaeaea)
-int COLOR14[3] = {234, 234, 234};
+const array<int, 3> COLOR14 = {234, 234, 234};
 
 /// @brief Color 15: Blanco puro (#ffffff)
-int COLOR15[3] = {255, 255, 255};
+const array<int, 3> COLOR15 = {255, 255, 255};
