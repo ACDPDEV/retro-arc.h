4b96cf9012868bae4d85222e825bf21f07d93996
ACDPDEV <iamacdp.dev@gmail.com>
2026-06-30 08:43:23 -0500

    feat: actualizar Common/Theme.h - const array<int,3> para todos los colores
    - Todos los 22 arrays como const array<int, 3>
    - Agrega #include <array> y using namespace std
    - Comentario fuente Omarchy

--- Common/Theme.h
+++ Common/Theme.h
@@ -1,32 +1,36 @@
 // Matte Black Theme desde Omarchy
 // https://omarchytheme.com/themes/matte-black/
 
+#include <array>
+
+using namespace std;
+
 // Base
-int BACKGROUND[3] = {18, 18, 18};      // #121212
-int FOREGROUND[3] = {190, 190, 190};    // #bebebe
-int ACCENT[3] = {230, 142, 13};     // #e68e0d
-int CURSOR[3] = {234, 234, 234};    // #eaeaea
+const array<int, 3> BACKGROUND = {18, 18, 18};      // #121212
+const array<int, 3> FOREGROUND = {190, 190, 190};    // #bebebe
+const array<int, 3> ACCENT = {230, 142, 13};     // #e68e0d
+const array<int, 3> CURSOR = {234, 234, 234};    // #eaeaea
 
 // Selection
-int SELECTION_BACKGROUND[3] = {81, 81, 81};       // #515151
-int SELECTION_FOREGROUND[3] = {190, 190, 190};    // #bebebe
+const array<int, 3> SELECTION_BACKGROUND = {81, 81, 81};       // #515151
+const array<int, 3> SELECTION_FOREGROUND = {190, 190, 190};    // #bebebe
 
 // Normal colors (0–7)
-int COLOR0[3]  = {51, 51, 51};      // #333333
-int COLOR1[3]  = {211, 95, 95};     // #D35F5F
-int COLOR2[3]  = {255, 193, 7};     // #FFC107
-int COLOR3[3]  = {185, 28, 28};     // #b91c1c
-int COLOR4[3]  = {230, 142, 13};    // #e68e0d
-int COLOR5[3]  = {211, 95, 95};     // #D35F5F
-int COLOR6[3]  = {190, 190, 190};   // #bebebe
-int COLOR7[3]  = {190, 190, 190};   // #bebebe
+const array<int, 3> COLOR0  = {51, 51, 51};      // #333333
+const array<int, 3> COLOR1  = {211, 95, 95};     // #D35F5F
+const array<int, 3> COLOR2  = {255, 193, 7};     // #FFC107
+const array<int, 3> COLOR3  = {185, 28, 28};     // #b91c1c
+const array<int, 3> COLOR4  = {230, 142, 13};    // #e68e0d
+const array<int, 3> COLOR5  = {211, 95, 95};     // #D35F5F
+const array<int, 3> COLOR6  = {190, 190, 190};   // #bebebe
+const array<int, 3> COLOR7  = {190, 190, 190};   // #bebebe
 
 // Bright colors (8–15)
-int COLOR8[3]  = {138, 138, 141};   // #8a8a8d
-int COLOR9[3]  = {185, 28, 28};     // #B91C1C
-int COLOR10[3] = {255, 193, 7};     // #FFC107
-int COLOR11[3] = {185, 10, 10};     // #b90a0a
-int COLOR12[3] = {245, 158, 11};    // #f59e0b
-int COLOR13[3] = {185, 28, 28};     // #B91C1C
-int COLOR14[3] = {234, 234, 234};   // #eaeaea
-int COLOR15[3] = {255, 255, 255};   // #ffffff
+const array<int, 3> COLOR8  = {138, 138, 141};   // #8a8a8d
+const array<int, 3> COLOR9  = {185, 28, 28};     // #B91C1C
+const array<int, 3> COLOR10 = {255, 193, 7};     // #FFC107
+const array<int, 3> COLOR11 = {185, 10, 10};     // #b90a0a
+const array<int, 3> COLOR12 = {245, 158, 11};    // #f59e0b
+const array<int, 3> COLOR13 = {185, 28, 28};     // #B91C1C
+const array<int, 3> COLOR14 = {234, 234, 234};   // #eaeaea
+const array<int, 3> COLOR15 = {255, 255, 255};   // #ffffff
