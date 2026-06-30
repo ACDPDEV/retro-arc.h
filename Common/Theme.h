67ca5708932bbfafc33956f9cbf9100c6177bfbe
ACDPDEV <iamacdp.dev@gmail.com>
2026-06-30 07:03:12 -0500

    feat: agregar const a arrays de colores en Common/Theme.h
    - Todas las variables de color ahora son const
    - Mejora seguridad y optimización del compilador

--- Common/Theme.h
+++ Common/Theme.h
@@ -2,31 +2,31 @@
 // https://omarchytheme.com/themes/matte-black/
 
 // Base
-int BACKGROUND[3] = {18, 18, 18};      // #121212
-int FOREGROUND[3] = {190, 190, 190};    // #bebebe
-int ACCENT[3] = {230, 142, 13};     // #e68e0d
-int CURSOR[3] = {234, 234, 234};    // #eaeaea
+const int BACKGROUND[3] = {18, 18, 18};      // #121212
+const int FOREGROUND[3] = {190, 190, 190};    // #bebebe
+const int ACCENT[3] = {230, 142, 13};     // #e68e0d
+const int CURSOR[3] = {234, 234, 234};    // #eaeaea
 
 // Selection
-int SELECTION_BACKGROUND[3] = {81, 81, 81};       // #515151
-int SELECTION_FOREGROUND[3] = {190, 190, 190};    // #bebebe
+const int SELECTION_BACKGROUND[3] = {81, 81, 81};       // #515151
+const int SELECTION_FOREGROUND[3] = {190, 190, 190};    // #bebebe
 
 // Normal colors (0–7)
-int COLOR0[3]  = {51, 51, 51};      // #333333
-int COLOR1[3]  = {211, 95, 95};     // #D35F5F
-int COLOR2[3]  = {255, 193, 7};     // #FFC107
-int COLOR3[3]  = {185, 28, 28};     // #b91c1c
-int COLOR4[3]  = {230, 142, 13};    // #e68e0d
-int COLOR5[3]  = {211, 95, 95};     // #D35F5F
-int COLOR6[3]  = {190, 190, 190};   // #bebebe
-int COLOR7[3]  = {190, 190, 190};   // #bebebe
+const int COLOR0[3]  = {51, 51, 51};      // #333333
+const int COLOR1[3]  = {211, 95, 95};     // #D35F5F
+const int COLOR2[3]  = {255, 193, 7};     // #FFC107
+const int COLOR3[3]  = {185, 28, 28};     // #b91c1c
+const int COLOR4[3]  = {230, 142, 13};    // #e68e0d
+const int COLOR5[3]  = {211, 95, 95};     // #D35F5F
+const int COLOR6[3]  = {190, 190, 190};   // #bebebe
+const int COLOR7[3]  = {190, 190, 190};   // #bebebe
 
 // Bright colors (8–15)
-int COLOR8[3]  = {138, 138, 141};   // #8a8a8d
-int COLOR9[3]  = {185, 28, 28};     // #B91C1C
-int COLOR10[3] = {255, 193, 7};     // #FFC107
-int COLOR11[3] = {185, 10, 10};     // #b90a0a
-int COLOR12[3] = {245, 158, 11};    // #f59e0b
-int COLOR13[3] = {185, 28, 28};     // #B91C1C
-int COLOR14[3] = {234, 234, 234};   // #eaeaea
-int COLOR15[3] = {255, 255, 255};   // #ffffff
+const int COLOR8[3]  = {138, 138, 141};   // #8a8a8d
+const int COLOR9[3]  = {185, 28, 28};     // #B91C1C
+const int COLOR10[3] = {255, 193, 7};     // #FFC107
+const int COLOR11[3] = {185, 10, 10};     // #b90a0a
+const int COLOR12[3] = {245, 158, 11};    // #f59e0b
+const int COLOR13[3] = {185, 28, 28};     // #B91C1C
+const int COLOR14[3] = {234, 234, 234};   // #eaeaea
+const int COLOR15[3] = {255, 255, 255};   // #ffffff
