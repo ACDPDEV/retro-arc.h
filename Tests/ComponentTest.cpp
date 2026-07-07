7a01672be3a7a44589cf92226ecd1bf3e912b9ed
ACDPDEV <iamacdp.dev@gmail.com>
2026-07-06 20:28:39 -0500

    test: update ComponentTest and add MainMenuTest
    - Add EnableTrueColor/EnableUTF8 calls to ComponentTest
    - Add Aligned.h and DrawText multiline test to ComponentTest
    - Add MainMenuTest for PresentationView/TransitionView/MainMenu

--- Tests/ComponentTest.cpp
+++ Tests/ComponentTest.cpp
@@ -1,9 +1,12 @@
 #include "../Common/Components/Select.h"
+#include "../Common/Components/Aligned.h"
 #include "../Common/Theme.h"
 
 using namespace std;
 
 int main() {
+    EnableTrueColor();
+    EnableUTF8();
     Clear();
     const vector<string> options = {
         "1. Option 1",
@@ -14,5 +17,7 @@ int main() {
     };
     int selected = 0;
     ComponentSelect(0, 0, BACKGROUND, SELECTION_BACKGROUND, options, selected, {"[>>>] ", ""});
+    DrawFillRectangle(0, 0, 100, 100, " ", FOREGROUND, BACKGROUND);
+    DrawText(AlignedX(0, 100, 10, "center"), 0, 10, -1, {"No se que escribir aquí", "Tampoco sé que escribir en esta línea"}, COLOR_DEFAULT, COLOR_DEFAULT);
     return 0;
 }
---
+++ Tests/MainMenuTest.cpp
@@ -0,0 +1,11 @@
+#include "../MainMenu/TransitionView.h"
+#include "../MainMenu/PresentationView.h"
+#include "../MainMenu/MainMenu.h"
+
+
+int main() {
+    PresentationView();
+    TransitionView();
+    MainMenu();
+    return 0;
+}
