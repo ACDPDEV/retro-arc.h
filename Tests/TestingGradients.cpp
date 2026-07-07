a792f9604c2727110bf2e9dd5075794986f98caa
ItsAluxCode <moirvalderrama@gmail.com>
2026-07-07 18:10:37 -0500

    (tests): tested with gradients

---
+++ Tests/TestingGradients.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "../Common/Color.h"
+#include "../Common/UnicodeGlyphs.h"
+using namespace std;
+
+int main() {
+    //10 pasos, comenzando en verde y acabando en rojo
+    vector<array<int, 3>> colors = Common::Gradient(100, {10, 67, 120}, {127, 0, 25});
+
+    for (int i = 0; i < colors.size(); i++) {
+    cout << Common::Color(colors[i], {-1,-1,-1}) << "\u2588";
+    }
+
+cout << Common::ResetColor();
+    return 0;
+}
