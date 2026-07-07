47e417b6fb9e6b29eec67beb72d4496f55c0513e
ItsAluxCode <moirvalderrama@gmail.com>
2026-07-07 18:12:44 -0500

    (tests): tested with gradients

--- Tests/TestingGradients.cpp
+++ Tests/TestingGradients.cpp
@@ -8,7 +8,7 @@ int main() {
     vector<array<int, 3>> colors = Common::Gradient(100, {10, 67, 120}, {127, 0, 25});
 
     for (int i = 0; i < colors.size(); i++) {
-    cout << Common::Color(colors[i], {-1,-1,-1}) << "\u2588";
+    cout << Common::Color(colors[i], {-1,-1,-1}) << "\u2588"; //█
     }
 
 cout << Common::ResetColor();
