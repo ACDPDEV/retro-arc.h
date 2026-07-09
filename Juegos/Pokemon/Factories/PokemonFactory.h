fa2dfc80ab972708ea215e8affe2ac3db0d65b73
nebadigital <aldaircolona22@gmail.com>
2026-07-09 15:19:42 -0500

    feat(pokemon): create buildteam function

---
+++ Juegos/Pokemon/Factories/PokemonFactory.h
@@ -0,0 +1,15 @@
+#include "../Models/Pokemon.h"
+
+namespace PokemonGame
+{
+    class PokemonFactory
+    {
+        private:
+            // static int totalDbPokemon;
+        public:
+            static PokemonGame::Pokemon* Create(int option)
+            {
+                
+            }
+    };
+}
