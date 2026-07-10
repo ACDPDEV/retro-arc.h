03adc228996a6acd67312c8248b1a3a7ef825f4c
nebadigital <aldaircolona22@gmail.com>
2026-07-09 20:46:17 -0500

    feat(pokemon): define basic player methods

--- Juegos/Pokemon/Models/Player.h
+++ Juegos/Pokemon/Models/Player.h
@@ -16,9 +16,9 @@ namespace PokemonGame
     
             std::string name;
     
-            std::vector<PokemonGame::Pokemon*> team;
+            std::vector<std::unique_ptr<PokemonGame::Pokemon>> team;
     
-            PokemonGame::Pokemon* activePokemon;
+            PokemonGame::Pokemon* activePokemon = nullptr;
     
             PokemonGame::Bag bag;
     
@@ -28,15 +28,95 @@ namespace PokemonGame
                 :name(name)
             {
             }
+
+            std::unique_ptr<PokemonGame::Command> Player::ChooseCommand(Battle& battle)
+            {
+                /**
+                 * TODO: UI para seleccionar la opción
+                 */
+                int option = 0; // ShowBattleMenu();  <<--- FUNCIÓN QUE MUESTRA LA VISTA PARA OBTENER LA OPCIÓN
+
+                switch(option)
+                {
+                    case 1:
+                        // return std::make_unique<AttackCommand>(ChooseMove());
+
+                    case 2:
+                        // return std::make_unique<BagCommand>(ChooseItem());
+
+                    case 3:
+                        // return std::make_unique<SwitchPokemonCommand>(ChoosePokemon());
+
+                    case 4:
+                        // return std::make_unique<RunCommand>();
+                }
+
+                return nullptr;
+            }
     
-            ~Player();
+            ~Player() = default;
+
+            std::string GetName()
+            {
+                return name;
+            }
+
+            bool HasLost()
+            {
+                /**
+                 * TODO: implementar
+                 */
+            }
+
+            /**
+             * @brief Elimina todos los Pokemon que pertenecen al jugador
+             */
+            void ClearTeam()
+            {
+                team.clear();
+                activePokemon = nullptr;
+            }
     
-            void addPokemon(PokemonGame::Pokemon* pokemon);
+            /**
+             * Obtiene la cantidad de pokemones dentro del equipo
+             * @returns La cantidad de pokemones dentro del equipo
+             */
+            int TeamSize() const
+            {
+                return team.size();
+            }
+
+            /**
+             * Agrega un pokemon al equipo del jugador
+             * @param pokemon Pokemon que se va a agregar al equipo
+             */
+            void AddPokemon(std::unique_ptr<PokemonGame::Pokemon> pokemon)
+            {
+                if (team.empty())
+                {
+                    activePokemon = pokemon.get();
+                }
+                team.push_back(std::move(pokemon));
+            }
+
+            PokemonGame::Pokemon* GetPokemon(size_t index)
+            {
+                return team.at(index).get();
+            }
     
-            void switchPokemon(int index);
+            void SwitchPokemon(size_t index)
+            {
+                activePokemon = team.at(index).get();
+            }
     
-            PokemonGame::Pokemon* getActivePokemon();
+            PokemonGame::Pokemon* GetActivePokemon()
+            {
+                return activePokemon;
+            }
     
-            PokemonGame::Bag& getBag();
+            PokemonGame::Bag& GetBag()
+            {
+                return bag;
+            }
     };
 }
