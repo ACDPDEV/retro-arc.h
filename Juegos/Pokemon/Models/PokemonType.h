c14a3c8cfc064dff3ed93479dfaa4b38adba1bd1
nebadigital <aldaircolona22@gmail.com>
2026-07-15 16:10:37 -0500

    feat(pokemon): create pokemon type enum and effectiveness matrix

---
+++ Juegos/Pokemon/Models/PokemonType.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Pokemon.h"
+#include "Move.h"
+
+namespace PokemonGame
+{
+    class PokemonGame::Move;
+    
+    enum class PokemonType
+    {
+        ACERO,
+        AGUA,
+        BICHO,
+        DRAGON,
+        ELECTRICO,
+        FANTASMA,
+        FUEGO,
+        HADA,
+        HIELO,
+        LUCHA,
+        NORMAL,
+        PLANTA,
+        PSIQUICO,
+        ROCA,
+        SINIESTRO,
+        TIERRA,
+        VENENO,
+        VOLADOR
+    };
+}
